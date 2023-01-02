d7fdcd96e06c7a8de87f777600d2d6136ab814c9
maryamsalamat <maryamsalamat47@gmail.com>
2023-01-02 20:04:38 +0330

    add formComponent

--- src/common/FormComponent.js
+++ src/common/FormComponent.js
@@ -1,1 +1,30 @@
+import { useEffect, useRef } from "react";
 
+const FormComponent = ({
+  styles,
+  submitHandler,
+  inputValue,
+  changeHandler,
+  text,
+}) => {
+  const inputRef = useRef();
+  useEffect(() => {
+    inputRef.current.focus();
+  }, []);
+  return (
+    <form className={styles.form} onSubmit={submitHandler}>
+      <input
+        type="text"
+        value={inputValue}
+        className={styles.input}
+        onChange={changeHandler}
+        ref={inputRef}
+      ></input>
+      <button type="submit" className={styles.btn}>
+        {text}
+      </button>
+    </form>
+  );
+};
+
+export default FormComponent;
--- src/components/EditTodo.js
+++ src/components/EditTodo.js
@@ -1,35 +1,29 @@
 import styles from "./EditTodo.module.css";
-import { useRef, useEffect, useState } from "react";
-
+import { useState } from "react";
+import FormComponent from "../common/FormComponent";
 const EditTodo = ({ todo, onEdit, setEdit }) => {
   const [inputValue, setInputValue] = useState(todo.text);
-  const inputRef = useRef();
-  useEffect(() => {
-    inputRef.current.focus();
-  }, []);
+
   const changeHandler = (e) => {
     setInputValue(e.target.value);
   };
   const submitHandler = (e) => {
     e.preventDefault();
+    if (inputValue === "") {
+      alert("enter todo");
+      return;
+    }
     onEdit(todo.id, inputValue);
     setEdit({ id: null, isCompleted: false, text: "" });
   };
   return (
-    <div className={styles.container}>
-      <form className={styles.form} onSubmit={submitHandler}>
-        <input
-          type="text"
-          value={inputValue}
-          className={styles.input}
-          onChange={changeHandler}
-          ref={inputRef}
-        ></input>
-        <button type="submit" className={styles.btn}>
-          edit
-        </button>
-      </form>
-    </div>
+    <FormComponent
+      styles={styles}
+      inputValue={inputValue}
+      submitHandler={submitHandler}
+      changeHandler={changeHandler}
+      text="edit"
+    />
   );
 };
 
